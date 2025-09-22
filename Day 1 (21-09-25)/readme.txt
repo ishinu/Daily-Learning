887ced6857f9d0ce07461396a79a05b971b9a05d
Shubham <tysmgroups@gmail.com>
2025-09-22 23:57:39 +0530

    Day 2, Mysql,Php Registration Form using xampp

--- Day 1 (21-09-25)/readme.txt
+++
@@ -1,7 +0,0 @@
-
-
-1. Implementing Bootstrap in HTML File.
-
-2. Understanding Breakpoints
-
-3. Navbar and Showcase area made.
---
+++ Day 2/connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; // Default password for XAMPP is an empty string
+$db_name = "phplogin";
+
+$conn = new mysqli($servername, $username, $password, $db_name, 3306); 
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connection Successful";
+?>
