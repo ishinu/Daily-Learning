f8c0727bca2fa094c5fe4ce58f32f7138e23b6b7
Shubham <tysmgroups@gmail.com>
2025-10-16 09:56:00 +0530

    Php Sql login form

--- Day 2/index.php
+++ Day 2/index.php
@@ -1,19 +1,44 @@
+<?php
+    include("connection.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <div id="form">
         <h1>Login Form</h1>
-        <form name="form" method="POST" action="">
+        <form name="form" method="POST" onsubmit="return isvalid()" action="login.php">
             <label>Username : </label>
-            <input type="text" id="user" name="user">
+            <input type="text" id="user" name="user"><br><br>
             <label>Password : </label>
-            <input type="password" id="pass" name="pass">
+            <input type="password" id="pass" name="pass"><br><br>
+            <input type="submit" id="btn" value="Login" name="submit"/>
         </form>
     </div>
+    <script>
+        function isvalid(){
+            var user = document.form.user.value;
+            var pass  = document.form.pass.value;
+            if(user.length=="" && pass.length==""){
+                alert("Username and password field is empty");
+                return false;
+            }
+            else{
+                if(user.length==""){
+                alert("Username field is empty");
+                return false;
+                }
+                if(pass.length==""){
+                alert("Password field is empty");
+                return false;
+                }
+            }
+        }
+    </script>
 </body>
 </html>
---
+++ Day 2/readme.txt
@@ -0,0 +1,7 @@
+1. Implementing Mysql, php using XAMPP.
+
+2. Create a folder in xampp/htdocs/phplogin/index.php
+
+3. Run xampp(Apache and Mysql) and open the same path in google.
+
+4. Make the connection.
